8c1fb6d6d95d2b1852d444df735799dfae59a7b5
MarinoGonzalez <josemarino.gonzalez@outlook.com>
2025-03-03 21:32:47 -0600

    Se implemento una funcion para agregar amigo, actualizar lista de amigos y un array para almacenar los nombres

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigosA = [];
+let mostarLista = document.getElementById('listaAmigos');
+let amigoSorteado = document.getElementById('resultado')
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+function limpiarLista() {
+    mostarLista.innerHTML = '';
+}
+
+
+function agregarAmigos () {
+    let agregarAmigoEnArray = document.getElementById('amigo').value;
+    console.log(agregarAmigoEnArray);
+
+    if (agregarAmigoEnArray === "" ){
+        alert('Por favor, inserte un nombre.');
+        return;
+    }else{
+        listaDeAmigosA.push(agregarAmigoEnArray);
+        limpiarCaja();
+        limpiarLista();
+        mostrarListaDeAmigos();
+    }
+}
+
+function mostrarListaDeAmigos() {
+
+for (let i = 0; i < listaDeAmigosA.length; i++) {
+    let nuevoLi = document.createElement('li');
+    nuevoLi.textContent = listaDeAmigosA[i];
+    mostarLista.appendChild(nuevoLi);  
+}
+}
+function sortearAmigo(){
+    if(listaDeAmigosA.length <= 1){
+        alert("Por Favor agrega dos o más amigos");
+        return;
+    }else{
+        let amigoSecreto = listaDeAmigosA[Math.floor(Math.random() * listaDeAmigosA.length)]
+        amigoSorteado.innerHTML = "El amigo secreto es:" + amigoSecreto;
+    }
+
+}
